60744bec9316568739d95068e3860d1033cdbfaf
Wesley Assis <wesleyassis2002@gmail.com>
2025-09-13 20:16:25 -0300

    Atualiza o controller com  mock de dados

--- src/main/java/com/xpto/CadastroDeFuncionarios/Usuarios/UsuarioController.java
+++ src/main/java/com/xpto/CadastroDeFuncionarios/Usuarios/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.xpto.CadastroDeFuncionarios.Usuarios;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,6 +10,38 @@ public class UsuarioController {
     @GetMapping("/boas-vindas")
     public String boasVindas(){
         return "Mensagem dessa rota";
+    }
+
+
+    // Adicionar Usuario (CREATE)
+    @PostMapping("/criar")
+    public String criarUsario(){
+        return "Usuario Criado";
+    }
+
+    // Mostrar todos os Usuario (READ)
+    @GetMapping("/todos")
+    public String motrarTodosUsuario(){
+        return "Mostrando o Usuario";
+    }
+
+    // Mostrar Usuarios por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosUsuariosPorId(){
+        return "Motrar Usuario por id";
+    }
+
+    // Alterar dados dos Usuarios (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarUsuarioPorId(){
+        return "Alterar Usuario por id";
+    }
+
+
+    // Deletar Usuario (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarUsuarioPorId(){
+        return "Usuario deletado por id";
     }
 
 }
